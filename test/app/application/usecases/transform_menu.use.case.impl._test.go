9e08d235c5cd286379c9557970e159cd6b75eabc
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2024-03-26 15:22:40 -0500

    feat[test]: add test to check use case

--- test/app/application/usecases/transform_menu.use.case.impl._test.go
+++ test/app/application/usecases/transform_menu.use.case.impl._test.go
@@ -145,4 +145,73 @@ func TestTransformUseCase(t *testing.T) {
 			t.Errorf("No existen items que coincidan con esa categoría")
 		}
 	})
+
+	t.Run("Conversión del precio de los items", func(t *testing.T) {
+		useCase := usecases.NewTransformMenuUseCaseImpl()
+		input := models.InputMenu{
+			Menus: []models.Menu{
+				{
+					ID:       "MenuId",
+					Title:    "Título",
+					Subtitle: "Subtítulo",
+				},
+			},
+			Categories: []models.Category{
+				{
+					ID:       "categoryId01",
+					Title:    "Título",
+					Subtitle: "Subtítulo",
+					Entities: []models.Entity{
+						{
+							ID:   "item01",
+							Type: "ITEM",
+						},
+						{
+							ID:   "item02",
+							Type: "NO_ITEM",
+						},
+					},
+				},
+				{
+					ID:       "categoryId02",
+					Title:    "Título",
+					Subtitle: "Subtítulo",
+				},
+			},
+			Items: []models.Item{
+				{
+					ID:    "item01",
+					Title: "Título",
+					PriceInfo: models.PriceInfo{
+						Price:     29.99,
+						CorePrice: 30.99,
+					},
+				},
+			},
+		}
+		output := useCase.Execute(&input)
+
+		if len(output) == 0 {
+			t.Errorf("Debería llegar por lo menos un menú")
+		}
+
+		categories := output[0].Categories
+		if len(categories) > 2 {
+			t.Errorf("No deberían llegar más de 2 categorías")
+		}
+
+		items := categories[0].Items
+		if len(items) == 0 {
+			t.Errorf("Debería llegar el item con id: 'item01'")
+		}
+
+		item := items[0]
+		if item.Price != 2999 {
+			t.Errorf("El valor de conversión de precio es incorrecto, debería ser 2999")
+		}
+
+		if item.CorePrice != 3099 {
+			t.Errorf("El valor de conversión de core es incorrecto, debería ser 3099")
+		}
+	})
 }
