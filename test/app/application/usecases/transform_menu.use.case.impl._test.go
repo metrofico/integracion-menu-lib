6742166c41f57ac314a6f0f43c2005b367844492
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2024-03-26 15:00:08 -0500

    feat[test]: add test to check that the input is not being received

--- test/app/application/usecases/transform_menu.use.case.impl._test.go
+++ test/app/application/usecases/transform_menu.use.case.impl._test.go
@@ -1,9 +1,22 @@
 package usecases
 
 import (
+	"fmt"
+	"shared-library/pkg/app/application/usecases"
 	"testing"
 )
 
 func TestTransformUseCase(t *testing.T) {
+	t.Run("No se envía nada en la firma del método", func(t *testing.T) {
+		useCase := usecases.NewTransformMenuUseCaseImpl()
+		defer func() {
+			if r := recover(); r == nil {
+				fmt.Println("La función no ha lanzado un panic:", r)
 
+				errorMsg := r.(string)
+				t.Errorf(errorMsg)
+			}
+		}()
+		useCase.Execute(nil)
+	})
 }
