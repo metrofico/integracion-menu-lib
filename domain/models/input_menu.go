df77675c2424a4bc2784977ec1038b74f59c63dd
xtdrack <joal.sala@gmail.com>
2024-03-27 13:42:37 -0500

    cambios menu uber

--- domain/models/input_menu.go
+++ domain/models/input_menu.go
@@ -1,78 +1,100 @@
 package models
 
-type (
-	InputMenu struct {
-		Menus      []Menu     `json:"menus"`
-		Categories []Category `json:"categories"`
-		Items      []Item     `json:"items"`
-	}
+type MenuData struct {
+	Menus          []Menu          `json:"menus"`
+	Categories     []Category      `json:"categories"`
+	Items          []Item          `json:"items"`
+	ModifierGroups []ModifierGroup `json:"modifierGroups"`
+}
 
-	Category struct {
-		ID       string   `json:"id"`
-		Title    string   `json:"title"`
-		Subtitle string   `json:"subtitle"`
-		Entities []Entity `json:"entities"`
-	}
+type Menu struct {
+	ID                  string                `json:"id"`
+	Title               string                `json:"title"`
+	Subtitle            string                `json:"subtitle"`
+	CategoryIDs         []string              `json:"category_ids"`
+	ServiceAvailability []ServiceAvailability `json:"service_availability"`
+}
 
-	Entity struct {
-		ID   string `json:"id"`
-		Type string `json:"type"`
-	}
+type ServiceAvailability struct {
+	DayOfWeek   string       `json:"day_of_week"`
+	TimePeriods []TimePeriod `json:"time_periods"`
+}
 
-	Item struct {
-		ID           string       `json:"id"`
-		ExternalData string       `json:"external_data"`
-		Title        string       `json:"title"`
-		Description  string       `json:"description"`
-		ImageURL     string       `json:"image_url"`
-		PriceInfo    PriceInfo    `json:"price_info"`
-		QuantityInfo QuantityInfo `json:"quantity_info"`
-	}
+type TimePeriod struct {
+	StartTime string `json:"start_time"`
+	EndTime   string `json:"end_time"`
+}
 
-	PriceInfo struct {
-		Price        float32      `json:"price"`
-		CorePrice    float32      `json:"core_price"`
-		PricedByUnit PricedByUnit `json:"priced_by_unit"`
-	}
+type Category struct {
+	ID       string   `json:"id"`
+	Title    string   `json:"title"`
+	Subtitle string   `json:"subtitle"`
+	Entities []Entity `json:"entities"`
+}
 
-	PricedByUnit struct {
-		MeasurementType string `json:"measurement_type"`
-		LengthUnit      int64  `json:"length_unit"`
-		WeightUnit      int64  `json:"weight_unit"`
-		VolumeUnit      int64  `json:"volume_unit"`
-	}
+type Entity struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
 
-	QuantityInfo struct {
-		Quantity Quantity `json:"quantity"`
-	}
+type Item struct {
+	ID               string           `json:"id"`
+	ExternalData     string           `json:"external_data"`
+	Title            string           `json:"title"`
+	Description      string           `json:"description"`
+	ImageURL         string           `json:"image_url"`
+	PriceInfo        PriceInfo        `json:"price_info"`
+	QuantityInfo     interface{}      `json:"quantity_info"` // Usado interface{} debido a la falta de campos específicos
+	ModifierGroupIDs ModifierGroupIDs `json:"modifier_group_ids"`
+}
 
-	Quantity struct {
-		MinPermitted           int64 `json:"min_permitted"`
-		MaxPermitted           int64 `json:"max_permitted"`
-		IsMinPermittedOptional int64 `json:"is_min_permitted_optional"`
-	}
+type PriceInfo struct {
+	Price            int          `json:"price"`
+	CorePrice        int          `json:"core_price"`
+	ContainerDeposit int          `json:"container_deposit"`
+	Overrides        interface{}  `json:"overrides"` // Usado interface{} debido a la falta de detalles
+	PricedByUnit     PricedByUnit `json:"priced_by_unit"`
+}
 
-	Menu struct {
-		ID                  string                `json:"id"`
-		Title               string                `json:"title"`
-		Subtitle            string                `json:"subtitle"`
-		CategoryIds         []string              `json:"category_ids"`
-		ServiceAvailability []ServiceAvailability `json:"service_availability"`
-	}
+type PricedByUnit struct {
+	MeasurementType string      `json:"measurement_type"`
+	LengthUnit      interface{} `json:"length_unit"`
+	WeightUnit      interface{} `json:"weight_unit"`
+	VolumeUnit      interface{} `json:"volume_unit"`
+}
 
-	ServiceAvailability struct {
-		DayOfWeek   string       `json:"day_of_week"`
-		TimePeriods []TimePeriod `json:"time_periods"`
-	}
+type ModifierGroupIDs struct {
+	IDs []string `json:"ids"`
+}
 
-	TimePeriod struct {
-		StartTime string `json:"start_time"`
-		EndTime   string `json:"end_time"`
-	}
-)
+type ModifierGroup struct {
+	QuantityInfo    QuantityInfo     `json:"quantity_info"`
+	Title           string           `json:"title"`
+	ExternalData    string           `json:"external_data"`
+	ID              string           `json:"id"`
+	ModifierOptions []ModifierOption `json:"modifier_options"`
+}
+
+type QuantityInfo struct {
+	Quantity Quantity `json:"quantity"`
+}
+
+type Quantity struct {
+	MinPermitted int `json:"min_permitted"`
+	MaxPermitted int `json:"max_permitted"`
+}
+
+type Title struct {
+	Translations map[string]string `json:"translations"`
+}
+
+type ModifierOption struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+}
 
 func (menu *Menu) ContainsCategory(categoryId string) bool {
-	slice := menu.CategoryIds
+	slice := menu.CategoryIDs
 	for _, v := range slice {
 		if v == categoryId {
 			return true
