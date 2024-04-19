f399fba7ce4caecd44ad4a7d06c60882ce085d6b
Joy <joy.lino@kfc.com.ec>
2024-04-19 13:06:10 -0500

    New changes core menu events, and object

--- pkg/shared/protobuff/core-menu/core-menu.pb.go
+++ pkg/shared/protobuff/core-menu/core-menu.pb.go
@@ -63,47 +63,94 @@ func (StreamsCoreMenu) EnumDescriptor() ([]byte, []int) {
 	return file_pkg_shared_protobuff_core_menu_proto_rawDescGZIP(), []int{0}
 }
 
-type StreamsCoreMenuSubjects int32
+type StreamsCoreMenuEvents int32
 
 const (
-	StreamsCoreMenuSubjects_GetMenuLegacy StreamsCoreMenuSubjects = 0
+	StreamsCoreMenuEvents_NewRequestAllSyncMenuEvent StreamsCoreMenuEvents = 0
 )
 
-// Enum value maps for StreamsCoreMenuSubjects.
+// Enum value maps for StreamsCoreMenuEvents.
 var (
-	StreamsCoreMenuSubjects_name = map[int32]string{
-		0: "GetMenuLegacy",
+	StreamsCoreMenuEvents_name = map[int32]string{
+		0: "NewRequestAllSyncMenuEvent",
 	}
-	StreamsCoreMenuSubjects_value = map[string]int32{
-		"GetMenuLegacy": 0,
+	StreamsCoreMenuEvents_value = map[string]int32{
+		"NewRequestAllSyncMenuEvent": 0,
 	}
 )
 
-func (x StreamsCoreMenuSubjects) Enum() *StreamsCoreMenuSubjects {
-	p := new(StreamsCoreMenuSubjects)
+func (x StreamsCoreMenuEvents) Enum() *StreamsCoreMenuEvents {
+	p := new(StreamsCoreMenuEvents)
 	*p = x
 	return p
 }
 
-func (x StreamsCoreMenuSubjects) String() string {
+func (x StreamsCoreMenuEvents) String() string {
 	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
 }
 
-func (StreamsCoreMenuSubjects) Descriptor() protoreflect.EnumDescriptor {
+func (StreamsCoreMenuEvents) Descriptor() protoreflect.EnumDescriptor {
 	return file_pkg_shared_protobuff_core_menu_proto_enumTypes[1].Descriptor()
 }
 
-func (StreamsCoreMenuSubjects) Type() protoreflect.EnumType {
+func (StreamsCoreMenuEvents) Type() protoreflect.EnumType {
 	return &file_pkg_shared_protobuff_core_menu_proto_enumTypes[1]
 }
 
-func (x StreamsCoreMenuSubjects) Number() protoreflect.EnumNumber {
+func (x StreamsCoreMenuEvents) Number() protoreflect.EnumNumber {
 	return protoreflect.EnumNumber(x)
 }
 
-// Deprecated: Use StreamsCoreMenuSubjects.Descriptor instead.
-func (StreamsCoreMenuSubjects) EnumDescriptor() ([]byte, []int) {
+// Deprecated: Use StreamsCoreMenuEvents.Descriptor instead.
+func (StreamsCoreMenuEvents) EnumDescriptor() ([]byte, []int) {
 	return file_pkg_shared_protobuff_core_menu_proto_rawDescGZIP(), []int{1}
+}
+
+type NewRequestAllSyncMenu struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *NewRequestAllSyncMenu) Reset() {
+	*x = NewRequestAllSyncMenu{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pkg_shared_protobuff_core_menu_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *NewRequestAllSyncMenu) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*NewRequestAllSyncMenu) ProtoMessage() {}
+
+func (x *NewRequestAllSyncMenu) ProtoReflect() protoreflect.Message {
+	mi := &file_pkg_shared_protobuff_core_menu_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use NewRequestAllSyncMenu.ProtoReflect.Descriptor instead.
+func (*NewRequestAllSyncMenu) Descriptor() ([]byte, []int) {
+	return file_pkg_shared_protobuff_core_menu_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *NewRequestAllSyncMenu) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
 }
 
 var File_pkg_shared_protobuff_core_menu_proto protoreflect.FileDescriptor
@@ -112,14 +159,17 @@ var file_pkg_shared_protobuff_core_menu_proto_rawDesc = []byte{
 	0x0a, 0x24, 0x70, 0x6b, 0x67, 0x2f, 0x73, 0x68, 0x61, 0x72, 0x65, 0x64, 0x2f, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x62, 0x75, 0x66, 0x66, 0x2f, 0x63, 0x6f, 0x72, 0x65, 0x2d, 0x6d, 0x65, 0x6e, 0x75,
 	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x63, 0x6f, 0x72, 0x65, 0x6d, 0x65, 0x6e, 0x75,
-	0x2a, 0x1f, 0x0a, 0x0f, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x73, 0x43, 0x6f, 0x72, 0x65, 0x4d,
-	0x65, 0x6e, 0x75, 0x12, 0x0c, 0x0a, 0x08, 0x53, 0x79, 0x6e, 0x63, 0x4d, 0x65, 0x6e, 0x75, 0x10,
-	0x00, 0x2a, 0x2c, 0x0a, 0x17, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x73, 0x43, 0x6f, 0x72, 0x65,
-	0x4d, 0x65, 0x6e, 0x75, 0x53, 0x75, 0x62, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x12, 0x11, 0x0a, 0x0d,
-	0x47, 0x65, 0x74, 0x4d, 0x65, 0x6e, 0x75, 0x4c, 0x65, 0x67, 0x61, 0x63, 0x79, 0x10, 0x00, 0x42,
-	0x20, 0x5a, 0x1e, 0x70, 0x6b, 0x67, 0x2f, 0x73, 0x68, 0x61, 0x72, 0x65, 0x64, 0x2f, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x66, 0x2f, 0x63, 0x6f, 0x72, 0x65, 0x2d, 0x6d, 0x65, 0x6e,
-	0x75, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x27, 0x0a, 0x15, 0x4e, 0x65, 0x77, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x41, 0x6c,
+	0x6c, 0x53, 0x79, 0x6e, 0x63, 0x4d, 0x65, 0x6e, 0x75, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x2a, 0x1f, 0x0a, 0x0f, 0x53, 0x74, 0x72,
+	0x65, 0x61, 0x6d, 0x73, 0x43, 0x6f, 0x72, 0x65, 0x4d, 0x65, 0x6e, 0x75, 0x12, 0x0c, 0x0a, 0x08,
+	0x53, 0x79, 0x6e, 0x63, 0x4d, 0x65, 0x6e, 0x75, 0x10, 0x00, 0x2a, 0x37, 0x0a, 0x15, 0x53, 0x74,
+	0x72, 0x65, 0x61, 0x6d, 0x73, 0x43, 0x6f, 0x72, 0x65, 0x4d, 0x65, 0x6e, 0x75, 0x45, 0x76, 0x65,
+	0x6e, 0x74, 0x73, 0x12, 0x1e, 0x0a, 0x1a, 0x4e, 0x65, 0x77, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x41, 0x6c, 0x6c, 0x53, 0x79, 0x6e, 0x63, 0x4d, 0x65, 0x6e, 0x75, 0x45, 0x76, 0x65, 0x6e,
+	0x74, 0x10, 0x00, 0x42, 0x20, 0x5a, 0x1e, 0x70, 0x6b, 0x67, 0x2f, 0x73, 0x68, 0x61, 0x72, 0x65,
+	0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x66, 0x2f, 0x63, 0x6f, 0x72, 0x65,
+	0x2d, 0x6d, 0x65, 0x6e, 0x75, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -135,9 +185,11 @@ func file_pkg_shared_protobuff_core_menu_proto_rawDescGZIP() []byte {
 }
 
 var file_pkg_shared_protobuff_core_menu_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
+var file_pkg_shared_protobuff_core_menu_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_pkg_shared_protobuff_core_menu_proto_goTypes = []interface{}{
-	(StreamsCoreMenu)(0),         // 0: coremenu.StreamsCoreMenu
-	(StreamsCoreMenuSubjects)(0), // 1: coremenu.StreamsCoreMenuSubjects
+	(StreamsCoreMenu)(0),          // 0: coremenu.StreamsCoreMenu
+	(StreamsCoreMenuEvents)(0),    // 1: coremenu.StreamsCoreMenuEvents
+	(*NewRequestAllSyncMenu)(nil), // 2: coremenu.NewRequestAllSyncMenu
 }
 var file_pkg_shared_protobuff_core_menu_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -152,19 +204,34 @@ func file_pkg_shared_protobuff_core_menu_proto_init() {
 	if File_pkg_shared_protobuff_core_menu_proto != nil {
 		return
 	}
+	if !protoimpl.UnsafeEnabled {
+		file_pkg_shared_protobuff_core_menu_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*NewRequestAllSyncMenu); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_pkg_shared_protobuff_core_menu_proto_rawDesc,
 			NumEnums:      2,
-			NumMessages:   0,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_pkg_shared_protobuff_core_menu_proto_goTypes,
 		DependencyIndexes: file_pkg_shared_protobuff_core_menu_proto_depIdxs,
 		EnumInfos:         file_pkg_shared_protobuff_core_menu_proto_enumTypes,
+		MessageInfos:      file_pkg_shared_protobuff_core_menu_proto_msgTypes,
 	}.Build()
 	File_pkg_shared_protobuff_core_menu_proto = out.File
 	file_pkg_shared_protobuff_core_menu_proto_rawDesc = nil
